378f264270c83cfe928cf17ac5f17bff88038490
eniacce <mesutaygn@gmail.com>
2019-09-06 07:42:54 -0400

    test

--- src/main/java/com/trnet/controller/Helloworld.java
+++ src/main/java/com/trnet/controller/Helloworld.java
@@ -24,7 +24,7 @@ public class Helloworld {
     @RequestMapping(value = "/firstPage", method = RequestMethod.GET)
     public String test(ModelMap mm) {
         String a="davar";
-        String bcd="deneme";
+        String bcd="denemes";
         long l = System.currentTimeMillis();
         List<Event> contacts = eventService.contactList();
         mm.addAttribute("test",a);
